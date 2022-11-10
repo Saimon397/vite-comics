497e8ee0df0bd6141affaa42d6439bb20c1250d1
Saimon397 <simonecarrabba@gmail.com>
2022-11-10 17:01:01 +0100

    Section1_finita

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-
+  <div class="container">
+    <h1>Content goes here!</h1>
   </div>
 </template>
 
@@ -13,4 +13,17 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/style/partials/mixins' as *;
 @use '../assets/style/partials/variabels' as *;
+
+.container {
+  @include dflex;
+  background-color: black;
+  justify-content: start;
+  max-width: 100%;
+  padding: 3rem;
+
+  h1 {
+    color: white;
+    margin-left: 180px;
+  }
+}
 </style>

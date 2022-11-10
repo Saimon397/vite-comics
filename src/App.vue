25424345ac7e26cad0845ca3f711f26094201c2a
Saimon397 <simonecarrabba@gmail.com>
2022-11-10 15:28:13 +0100

    Start_2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
-export default {
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
+export default {
+  components: { HeaderComponent, MainComponent, FooterComponent }
 }
 </script>
 

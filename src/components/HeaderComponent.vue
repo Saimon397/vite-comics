8b3b8d81b15afa70eb9b66fe5aa08f2a89724148
Saimon397 <simonecarrabba@gmail.com>
2022-11-10 15:51:48 +0100

    Start_header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,7 +6,63 @@
 
 <script>
 export default {
-
+  name: 'HeaderComponent',
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#nogo",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Tv",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#nogo",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#nogo",
+          current: false,
+        },
+      ]
+    }
+  }
 }
 </script>
 
